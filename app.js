94a31f55bc73f1071c2bf918b36c5bf47169f948
Taras Noga <taraskin2012@gmail.com>
2023-12-14 14:02:29 +0200

    rewrite 404 page to handlebars page

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
-
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
