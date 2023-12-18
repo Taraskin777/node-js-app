e075d47777c139113f51d00bf858e6254f3f15c7
Taras Noga <taraskin2012@gmail.com>
2023-12-18 17:55:54 +0200

    add layout

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine('hbs', expressHbs());
-app.set('view engine', 'hbs');
-app.set('views', 'views');
+app.engine(
+  "hbs",
+  expressHbs({
+    layoutsDir: "views/layouts",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "views");
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
@@ -19,7 +26,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found!' });
+  res.status(404).render("404", { pageTitle: "Page Not Found!" });
 });
 
 app.listen(3000);
