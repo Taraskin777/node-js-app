467fc26ca79bc489d17cbaca5566c3f29805049c
Taras Noga <taraskin2012@gmail.com>
2023-12-30 22:20:04 +0200

    add rendering the product detail view

--- controllers/shop.js
+++ controllers/shop.js
@@ -11,15 +11,13 @@ exports.getProducts = (req, res, next) => {
 };
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  console.log(prodId);
-  res.redirect(`/${prodId}`);
-  // Product.fetchAll((products) => {
-  //   res.render("shop/product-list", {
-  //     prods: products,
-  //     pageTitle: "All Products",
-  //     path: "/products",
-  //   });
-  // });
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+    });
+  });
 };
 
 exports.getIndex = (req, res, next) => {
@@ -46,10 +44,9 @@ exports.getOrders = (req, res, next) => {
   });
 };
 
-
-exports.getCheckout = (req, res, next) => { 
+exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     path: "/checkout",
     pageTitle: "Checkout",
-  })
-}
+  });
+};
