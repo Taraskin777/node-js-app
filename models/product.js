820ecaa5a7143b1c21bba0f0cf04b6cc3d26ab7c
Taras Noga <taraskin2012@gmail.com>
2024-04-23 20:11:22 +0300

    add product to db

--- models/product.js
+++ models/product.js
@@ -17,12 +17,17 @@ module.exports = class Product {
     this.price = price;
   }
 
-  save() {}
+  save() {
+    return db.execute(
+      'INSERT INTO products(title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    );
+  }
 
   static deleteById(id) {}
 
   static fetchAll(cb) {
-   return db.execute('SELECT * FROM products');
+    return db.execute('SELECT * FROM products');
   }
 
   static findById(id) {}
