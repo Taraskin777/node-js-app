a5977c1adf4ebf66ec904299497aea0481ec550d
Taras Noga <taraskin2012@gmail.com>
2024-04-23 15:01:31 +0300

    retrieve data from db

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,34 +1,38 @@
-const Product = require("../models/product");
-const Cart = require("../models/cart");
+const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render("shop/product-list", {
+    res.render('shop/product-list', {
       prods: products,
-      pageTitle: "All Products",
-      path: "/products",
+      pageTitle: 'All Products',
+      path: '/products',
     });
   });
 };
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, (product) => {
-    res.render("shop/product-detail", {
+    res.render('shop/product-detail', {
       product: product,
       pageTitle: product.title,
-      path: "/products",
+      path: '/products',
     });
   });
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/index", {
-      prods: products,
-      pageTitle: "Shop",
-      path: "/",
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render('shop/index', {
+        prods: rows,
+        pageTitle: 'Shop',
+        path: '/',
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  });
 };
 
 exports.getCart = (req, res, next) => {
@@ -43,9 +47,9 @@ exports.getCart = (req, res, next) => {
           cartProducts.push({ productData: product, qty: cartProductData.qty });
         }
       }
-      res.render("shop/cart", {
-        path: "/cart",
-        pageTitle: "Your Cart",
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
         products: cartProducts,
       });
     });
@@ -57,27 +61,27 @@ exports.postCart = (req, res, next) => {
   Product.findById(prodId, (product) => {
     Cart.addProduct(prodId, product.price);
   });
-  res.redirect("/cart");
+  res.redirect('/cart');
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId, (product) => {
     Cart.deleteProduct(prodId, product.price);
-    res.redirect("/cart");
+    res.redirect('/cart');
   });
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders',
   });
 };
 
 exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    path: "/checkout",
-    pageTitle: "Checkout",
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout',
   });
 };
