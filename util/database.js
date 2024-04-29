8415fa229b1f89a187f04fc5f50e46f239e4432d
Taras Noga <taraskin2012@gmail.com>
2024-04-29 13:42:49 +0300

    add product to db

--- util/database.js
+++ util/database.js
@@ -1,15 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    'mongodb+srv://tarasnoga777:pass17101980@cluster0.qlklvkd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    'mongodb+srv://tarasnoga777:pass17101980@cluster0.qlklvkd.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0'
   )
     .then((client) => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
