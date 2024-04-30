4844bf6fb05d9c4efd6d31a32e4f3004b144daac
Taras Noga <taraskin2012@gmail.com>
2024-04-30 14:20:34 +0300

    update product

--- models/product.js
+++ models/product.js
@@ -1,12 +1,13 @@
-const { ObjectId } = require('mongodb');
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
-    this.description = description;
     this.price = price;
+    this.description = description;
     this.imageUrl = imageUrl;
+    this._id = new mongodb.ObjectId(id);
   }
 
   save() {
@@ -14,7 +15,22 @@ class Product {
     return db
       .collection('products')
       .insertOne(this)
-      .then((result) => console.log(result))
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  update() {
+    const db = getDb();
+    return db
+      .collection('products')
+      .updateOne({ _id: this._id }, { $set: this })
+      .then((result) => {
+        console.log(result);
+      })
       .catch((err) => {
         console.log(err);
       });
@@ -35,17 +51,30 @@ class Product {
       });
   }
 
-  static fetchById(prodId) {
+  static findById(prodId) {
     const db = getDb();
     return db
       .collection('products')
-      .find({ _id: ObjectId.createFromHexString(prodId) })
+      .find({ _id: new mongodb.ObjectId(prodId) })
       .next()
       .then((product) => {
         console.log(product);
         return product;
       })
-      .catch(() => {
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        console.log('Deleted');
+      })
+      .catch((err) => {
         console.log(err);
       });
   }
