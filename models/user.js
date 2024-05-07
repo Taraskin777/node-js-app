596722c7695dad4e412465358bd5bd6d9b9cfadc
Taras Noga <taraskin2012@gmail.com>
2024-05-07 17:50:59 +0300

    add orders

--- models/user.js
+++ models/user.js
@@ -86,6 +86,21 @@ class User {
       );
   }
 
+  addOrder() {
+    const db = getDb();
+    db.collection('orders')
+      .insertOne(this.cart)
+      .then((result) => {
+        this.cart = { items: [] };
+        return db
+          .collection('users')
+          .updateOne(
+            { _id: new ObjectId(this._id) },
+            { $set: { cart: { items: [] } } }
+          );
+      });
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
