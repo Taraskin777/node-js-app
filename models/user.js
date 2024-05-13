57b640f6ce72499b45da6dd0f01cf9314cd6a86d
Taras Noga <taraskin2012@gmail.com>
2024-05-13 17:30:33 +0300

    add order

--- models/user.js
+++ models/user.js
@@ -57,6 +57,11 @@ userSchema.methods.removeFromCart = function (productId) {
   return this.save();
 };
 
+
+userSchema.methods.addOrder = function() {
+  
+}
+
 module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require('mongodb');
