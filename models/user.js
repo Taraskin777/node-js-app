7e72a26643072cfc8df875cd8dd1fd8becb8157b
Taras Noga <taraskin2012@gmail.com>
2024-05-13 17:40:50 +0300

    clear cart after order

--- models/user.js
+++ models/user.js
@@ -57,10 +57,12 @@ userSchema.methods.removeFromCart = function (productId) {
   return this.save();
 };
 
+userSchema.methods.clearCart = function () {
+  this.cart = { items: [] };
+  return this.save();
+};
 
-userSchema.methods.addOrder = function() {
-  
-}
+userSchema.methods.addOrder = function () {};
 
 module.exports = mongoose.model('User', userSchema);
 
