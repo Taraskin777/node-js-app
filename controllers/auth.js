6356adfbb96e510b9b8a42b51a9e589ffba184ad
Taras Noga <taraskin2012@gmail.com>
2024-11-07 17:19:37 +0200

    add signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,38 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-  // const isLoggedIn = req.get('Cookie').trim().split('=')[1];
-  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false,
+    isAuthenticated: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
   User.findById('663e417b411fc0af496857b9')
-    .then((user) => {
+    .then(user => {
       req.session.isLoggedIn = true;
       req.session.user = user;
-      req.session.save((err) => {
+      req.session.save(err => {
         console.log(err);
         res.redirect('/');
       });
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
 
+exports.postSignup = (req, res, next) => {};
+
 exports.postLogout = (req, res, next) => {
-  req.session.destroy((err) => {
+  req.session.destroy(err => {
     console.log(err);
     res.redirect('/');
   });
