b8a82aabfc98a4576a9dc049071269fc51f99682
Taras Noga <taraskin2012@gmail.com>
2024-11-11 13:27:12 +0200

    add csrf to all post requests

--- controllers/shop.js
+++ controllers/shop.js
@@ -36,9 +36,7 @@ exports.getIndex = (req, res, next) => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/',
-        isAuthenticated: req.session.isLoggedIn,
-        csrfToken: req.csrfToken(),
+        path: '/'
       });
     })
     .catch((err) => console.log(err));
@@ -91,7 +89,7 @@ exports.postOrder = (req, res, next) => {
       });
       const order = new Order({
         user: {
-          name: req.user.name,
+          email: req.user.email,
           userId: req.user,
         },
         products: products,
